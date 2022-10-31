b3b6237988bccc2a39195fb7bb3df260a150ee93
hongxiang.gao <hongxiang.gao@retech.work>
2022-10-31 18:13:44 +0800

    feat: 初始化构建脚本 OC-60

--- README.md
+++ README.md
@@ -6,6 +6,28 @@ pnpm install
 
 ## 启动
 
+### 启动所有
+
 ```js
 pnpm run dev
+```
+
+### 指定页面启动
+
+```js
+pnpm run dev --page=test  //test为src下面的mjml文件名称，无须后缀
+```
+
+## 构建
+
+### 构建所有
+
+```js
+pnpm run build
+```
+
+### 指定页面构建
+
+```js
+pnpm run build --page=test  //test为src下面的mjml文件名称，无须后缀
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,47 @@
-const gulp = require("gulp")
-const connect = require("gulp-connect") // 起server服务
-const mjml = require("gulp-mjml")
-// const del = require("del") // 清空目录
-
-// // 清空dist目录
-// gulp.task("clean", async () => {
-//   await del(["./dist"])
-// })
-
+const gulp = require('gulp');
+const connect = require('gulp-connect'); // 起server服务
+const mjml = require('gulp-mjml');
+const shell = require('shelljs');
+const argv = require('yargs-parser')(process.argv.slice(2));
+console.log('argv=>', argv.page);
 // mjml
-gulp.task("html:mjml", () => {
-  console.log("222=>", 222)
-  return gulp.src("./src/test.mjml").pipe(mjml()).pipe(gulp.dest("./dist"))
-})
+const srcFiles = argv?.page ? argv?.page + '.mjml' : '*';
+const src = `./src/${srcFiles}`;
 
-// html:dev task，用于开发环境下，浏览器自动刷新
-gulp.task("html:dev", async () => {
-  await htmlMin().pipe(connect.reload())
-})
+// 清空dist目录
+gulp.task('clean:dist', async () => {
+  await shell.rm('-rf', './dist');
+});
+
+// 清空static目录
+gulp.task('clean:static', async () => {
+  await shell.rm('-rf', './static');
+});
+gulp.task('html:dev', () => {
+  return gulp.src(src).pipe(mjml()).pipe(gulp.dest('./static'));
+});
+
 // html:build task，用于生产环境
-gulp.task("html:build", async () => {
-  await htmlMin()
-})
+gulp.task('html:build', async () => {
+  return gulp.src(src).pipe(mjml()).pipe(gulp.dest('./dist'));
+});
 
 // server任务，目录为dist，入口文件为dist/index.html，port 8080
-gulp.task("server", () => {
+gulp.task('server', () => {
   connect.server({
-    root: "./dist",
+    root: './dist',
     port: 8080,
     livereload: true,
     open: true,
-  })
-})
+  });
+});
 
 // watch任务，监听源文件变化，执行对应开发任务
-gulp.task("watch", () => {
+gulp.task('watch', () => {
   // gulp.watch("./src", gulp.series("html:dev"))
-  gulp.watch("./src", gulp.series("html:mjml"))
-})
+  gulp.watch('./src', gulp.series('html:dev'));
+});
 
 // dev任务，启动开发环境
-gulp.task("dev", gulp.series(gulp.parallel("watch", "server", "html:mjml")))
+gulp.task('dev', gulp.series('clean:static', gulp.parallel('watch', 'server', 'html:dev')));
+gulp.task('build', gulp.series('clean:dist', 'html:build'));
